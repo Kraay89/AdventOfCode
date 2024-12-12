c2f553dddf037b8f1a13a35ac6a06affa7ad073d
Harmen <hwkraayenbrink@gmail.com>
2024-12-12 22:19:53 +0100

    Further refactoring steps

--- PuzzleTools/Tools.py
+++ PuzzleTools/Tools.py
@@ -5,9 +5,13 @@ import re
 
 
 def getToday():
-    return datetime.datetime.today().day
+    today = datetime.datetime.today()
+    if today.month == 12:
+        return today.day
+    else:
+        raise ValueError
 
-def getPuzzleFiles():
+def getPuzzleSolutions():
     basepath = Path(os.getcwd())
     puzzleFolder = basepath /"PuzzleSolutions"
     pattern = re.compile('Day\d+\.py')
@@ -18,6 +22,11 @@ def getInputFiles():
     puzzleFolder = basepath /"PuzzleInputs"
     pattern = re.compile('Day\d+\.py')
     return [f for f in os.listdir(puzzleFolder) if os.path.isfile(os.path.join(puzzleFolder, f)) and pattern.match(f)]
+
+def projectRefresh():
+    inputs = getInputFiles()
+    solutions = getPuzzleSolutions()
+    
 
 def startNewDay():
     pass
